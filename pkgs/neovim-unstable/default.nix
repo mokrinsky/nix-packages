11333c3367f331e8a18e6a8fcc894a81a42ac8a8
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-03-31 23:23:03 +0400

    neovim-unstable remove patch merged into master

--- pkgs/neovim-unstable/default.nix
+++ pkgs/neovim-unstable/default.nix
@@ -11,4 +11,11 @@ neovim-unwrapped.overrideAttrs (oldAttrs: rec {
     rev = "5bf2f4b3c29fdab72044ddce74f06cb45fe9401c";
     hash = "sha256-Y3XAV9bEOLJgs+u9mwQowLASFoVJjpobC7CXCe05Mzk=";
   };
+
+  patches = [
+    # introduce a system-wide rplugin.vim in addition to the user one
+    # necessary so that nix can handle `UpdateRemotePlugins` for the plugins
+    # it installs. See https://github.com/neovim/neovim/issues/9413.
+    ./system_rplugin_manifest.patch
+  ];
 })
